7f391c3545d26af865cafd9724df6676a07d0548
TiciB <tebezerra@gmail.com>
2022-02-14 21:54:31 -0300

    Adicionando o carrossel na home

--- agenciaviagens/src/App.js
+++ agenciaviagens/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './componentes/Header';
 import Footer from './componentes/Footer';
+import Home from './componentes/Home';
 function App() {
   return (
     <div className="App">
      <Header />
+     <Home />
      <Footer />
     </div>
   );
--- agenciaviagens/src/componentes/Footer.js
+++ agenciaviagens/src/componentes/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
+import pagamento from './pagamento.png';
+
 class Footer extends React.Component{
     render() {
         return(
         <div>
             <center>
             <h4>FORMAS DE PAGAMENTO</h4>
-            <img src="./imagens/formas-de-pagamento.png" alt="Formas de pagamento" width="450px" />
+            <img src={ pagamento }alt="Formas de pagamento" width="450px" />
         </center>
         <center style={{fontFamily: "monospace", color: "purple"}}>&copy;Tici Bezerra</center>
         </div>    
