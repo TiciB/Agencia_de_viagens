a73779d0606eec17b85be1191268088008b0f153
TiciB <tebezerra@gmail.com>
2022-02-14 21:21:27 -0300

    add projeto no react

---
+++ agenciaviagens/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './componentes/Header';
+import Footer from './componentes/Footer';
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ agenciaviagens/src/componentes/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+class Footer extends React.Component{
+    render() {
+        return(
+        <div>
+            <center>
+            <h4>FORMAS DE PAGAMENTO</h4>
+            <img src="./imagens/formas-de-pagamento.png" alt="Formas de pagamento" width="450px" />
+        </center>
+        <center style={{fontFamily: "monospace", color: "purple"}}>&copy;Tici Bezerra</center>
+        </div>    
+        
+        );
+    }
+}
+export default Footer;
